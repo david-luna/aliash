34773fb5a46cbfb287db7baa124789afab8721f4
David Luna <luna.bistuer@gmail.com>
2024-01-03 21:15:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [ -d "~/.zsh_tools" ]; then
 else
   echo "Installing ZSH tools"
   mkdir ~/.zsh_tools
-  mkdir ~/.zsh_tools/git
+  mkdir ~/.zsh_tools/envs
 fi
 
 cp ./git/cli.sh ~/.zsh_tools/git/cli.sh
