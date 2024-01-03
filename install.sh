0be6831c8cf0102ab6c75e462298b07cf2f2ea48
David Luna <luna.bistuer@gmail.com>
2024-01-03 21:31:25 +0100

    chore: update install script

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
-if [ -d "~/.zsh_tools" ]; then
+BASE_URL="https://raw.githubusercontent.com/david-luna/shell-aliases/main/"
+
+# Create folder
+if [ -d "~/.sh_tools" ]; then
   # TODO: maybe cleanup folder?
-  echo "Updating ZSH tools"
+  echo "Updating SH tools"
 else
-  echo "Installing ZSH tools"
-  mkdir ~/.zsh_tools
-  mkdir ~/.zsh_tools/envs
+  echo "Installing SH tools"
+  mkdir ~/.sh_tools
+  mkdir ~/.sh_tools/envs
 fi
 
-cp ./git/cli.sh ~/.zsh_tools/git/cli.sh
+# Copy sources
+#curl -o ~/.sh_tools/colors.sh "${BASE_URL}colors.sh"
+echo "${BASE_URL}colors.sh"
+
+# Add to rc/profile files
