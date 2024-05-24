525ffe69f94873ecdc61ef2bacf29aafe4dc7c8f
David Luna <david@David-MacBook-Pro.local>
2024-05-24 09:05:03 +0200

    chore: move sources to separate folder

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# shell-aliases
+# aliash
 set of alias for my shell
--- install.sh
+++ install.sh
@@ -1,25 +1,44 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-BASE_PATH="~/.sh_tools"
-BASE_URL="https://raw.githubusercontent.com/david-luna/shell-aliases/main/"
+# Credits to https://www.youtube.com/watch?v=mSXOYhfDFYo
 
-# Create folder
+function ask() {
+  read -p "$1 (Y/n):" response
+  [ -z "$response" ] || [ "$response" = "y" ]
+}
 
-if [ ! -d "$BASE_PATH" ]; then
-  echo "Installing SH tools"
-  mkdir ~/.sh_tools
-  mkdir ~/.sh_tools/envs
-fi
+fullpath=$(realpath ".")
 
-# Copy sources to a single file
-rm -f ~/.sh_tools/aliases.sh
-echo "#!/usr/bin/env bash" >> ~/.sh_tools/aliases.sh
+ echo "source $fullpath"
 
-files=("colors" "git-cli" "with-env")
-for f in "${files[@]}"
-do
-  echo "Downloading ${BASE_URL}${f}.sh"
-  curl "${BASE_URL}${f}.sh" >> ~/.sh_tools/aliases.sh
-done
+# for file in sources/*
+# do
+#   fullpath=$(realpath $file)
+#   if ask "Source ${file}?"; then
+#     echo "source $fullpath" >> ~/.zshrc
+#   fi
+# done
 
-# TODO: Add to rc/profile files
+# BASE_PATH="~/.sh_tools"
+# BASE_URL="https://raw.githubusercontent.com/david-luna/shell-aliases/main/"
+
+# # Create folder
+
+# if [ ! -d "$BASE_PATH" ]; then
+#   echo "Installing SH tools"
+#   mkdir ~/.sh_tools
+#   mkdir ~/.sh_tools/envs
+# fi
+
+# # Copy sources to a single file
+# rm -f ~/.sh_tools/aliases.sh
+# echo "#!/usr/bin/env bash" >> ~/.sh_tools/aliases.sh
+
+# files=("colors" "git-cli" "with-env")
+# for f in "${files[@]}"
+# do
+#   echo "Downloading ${BASE_URL}${f}.sh"
+#   curl "${BASE_URL}${f}.sh" >> ~/.sh_tools/aliases.sh
+# done
+
+# # TODO: Add to rc/profile files
